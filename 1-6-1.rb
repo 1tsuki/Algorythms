9a4dae9f99e964639b6b120f644bdd5a43c17d00
astrider <astrider.com@gmail.com>
2015-08-14 16:44:28 +0900

    refactored some coding rules

--- 1-6-1.rb
+++ 1-6-1.rb
@@ -1,22 +1,25 @@
 ####################
 # main code block
 ####################
-def solve(args)
-  # sort args desc
-  sorted_args = args.sort {|a, b| (-1) * (a <=> b) }
-  sorted_args.each_with_index do |arg, i|
+def solve
+  sorted_a = @a.sort {|a, b| (-1) * (a <=> b) }
+  
+  sorted_a.each_with_index do |arg, i|
     # quit if array length exceeded
-    if i+3 > sorted_args.length then
-      return 0
+    if i+3 > sorted_a.length then
+      puts 0
+      return
     else
-      if is_triangible(arg, sorted_args[i+1], sorted_args[i+2]) then
-        return arg + sorted_args[i+1] + sorted_args[i+2]
+      if is_triangible?(arg, sorted_a[i+1], sorted_a[i+2]) then
+        puts arg + sorted_a[i+1] + sorted_a[i+2]
+        return
       end
     end
   end
+  puts 0
 end
 
-def is_triangible(a, b, c)
+def is_triangible?(a, b, c)
   array = [a,b,c].sort
   return (a<b+c) ? true : false
 end
@@ -25,10 +28,25 @@ end
 # code block for local testing
 ####################
 if __FILE__ == $0
-    n = gets.chomp.to_i
-    args = []
-    for num in 1..n do
-      args[num-1] = gets.chomp.to_i
+    @n = gets.chomp.to_i
+    @a = []
+    for i in 1..@n do
+      @a[i-1] = gets.chomp.to_i
     end
-    puts solve(args)
+    solve
 end
+
+####################
+# sample input
+####################
+# 5
+# 2
+# 3
+# 4
+# 5
+# 10
+
+####################
+# expected output
+####################
+# 12
